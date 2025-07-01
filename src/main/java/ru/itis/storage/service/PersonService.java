10a319f2dec641d14df74cc32ba7c0d0da48299d
Илья Иванов <mistika1092@gmail.com>
2025-07-02 00:47:19 +0300

    added cashing

--- src/main/java/ru/itis/storage/service/PersonService.java
+++ src/main/java/ru/itis/storage/service/PersonService.java
@@ -1,6 +1,8 @@
 package ru.itis.storage.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import ru.itis.storage.model.PersonEntity;
 import ru.itis.storage.repository.PersonRepository;
@@ -12,10 +14,12 @@ import java.util.List;
 public class PersonService {
     private final PersonRepository personRepository;
 
+    @Cacheable("persons")
     public PersonEntity savePerson(PersonEntity person) {
         return personRepository.save(person);
     }
 
+    @CacheEvict(value = "persons", allEntries = true)
     public List<PersonEntity> getAllPersons() {
         return personRepository.findAll();
     }
